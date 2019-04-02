9dc09d6a0278f0e1452f44dc74e8dd29ca5cf9d5
Frito <malline@Frito-Kubuntu.(none)>
2019-04-01 21:50:30 -0500

    End of code stream 5

---
+++ ui/src/app/layout/Site.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Site = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    min-hight: 100vh;
+`
+
+export default Site
